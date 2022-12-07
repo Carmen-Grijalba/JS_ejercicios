160f88708cdc61aac7dde66df0d502863ea339df
Carmen <100070201+Carmen-Grijalba@users.noreply.github.com>
2022-12-07 18:35:57 +0100

    Create index.js

---
+++ Ejercicio_14/index.js
@@ -0,0 +1,7 @@
+// Vincula un evento de tipo "click" al botón anterior, que muestre una alerta en el navegador "click en el botón"
+
+const botonAlerta = document.querySelector("#btn")
+
+botonAlerta.addEventListener("click", () => {
+    alert("Click en el botón")
+})
